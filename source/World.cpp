8080291d2b00202f03889dd10331a2f8b90087b3
Luke K Schubert <luke5@umbc.edu>
2015-08-10 13:34:07 -0400

    Signed-off-by: Luke K Schubert <luke5@umbc.edu>

--- source/World.cpp
+++ source/World.cpp
@@ -42,5 +42,42 @@ void World::displayWorld(){
 
 
 void World::update(){
+	age ++;
+	vector<double> inputs;
+	inputs.clear();
+	inputs.push_back((double)Math.abs(plant[0] - bunny[0]) / (double) size);
+	inputs.push_back((double)Math.abs(plant[1] - bunny[1]) / (double) size);
+	bunnyBrain.setInputs(inputs);
+	int decision = bunnyBrain.think();
+	int secondThought = bunnyBrain.secondThought();
+	int tempA[2];
+	int tempB[2];
+	switch (decision){
+		case 0: (bunny[0] > 0) ? tempA[0] = bunny[0] - 1 : tempA[0] = bunny[0];
+			break;
+		case 1: (bunny[1] > 0) ? tempA[1] = bunny[1] - 1 : tempA[1] = bunny[1];
+			break;	
+		case 2: (bunny[0] < size) ? tempA[0] = bunny[0] + 1 : tempA[0] = bunny[0];
+			break;
+		case 2: (bunny[1] < size) ? tempA[1] = bunny[1] + 1 : tempA[1] = bunny[1];
+			break;
+	}
+	
+	switch (secondThought){
+		case 0: (bunny[0] > 0) ? tempB[0] = bunny[0] - 1 : tempB[0] = bunny[0];
+			break;
+		case 1: (bunny[1] > 0) ? tempB[1] = bunny[1] - 1 : tempB[1] = bunny[1];
+			break;	
+		case 2: (bunny[0] < size) ? tempB[0] = bunny[0] + 1 : tempB[0] = bunny[0];
+			break;
+		case 2: (bunny[1] < size) ? tempB[1] = bunny[1] + 1 : tempB[1] = bunny[1];
+			break;
+	}
+	
+	bunny = tempA;
+	
+	if(Math.abs(tempA[0] - plant[0]) + Math.abs(tempA[1] - plant[1]) > Math.abs(tempB[0] - plant[0]) + Math.abs(tempB[1] - plant[1])){
+		bunnyBrain.adapt();
+	}
 	
 }
